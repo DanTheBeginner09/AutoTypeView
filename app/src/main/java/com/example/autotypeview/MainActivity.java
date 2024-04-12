fc919d43c4c72c61f1ea07981ea65dcfe6ca3a84
Lenov <villanuevadanzen090923@gmail.com>
2024-04-12 23:39:41 +0800

    first commit

---
+++ app/src/main/java/com/example/autotypeview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.autotypeview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        String[] items = getResources().getStringArray(R.array.pet);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+    }
+}
